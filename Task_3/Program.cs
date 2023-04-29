e4302be1eb01591007e635399bc939492a73d92a
*Elizarov <Super.elizarov1451@mail.ru>
2023-04-29 21:33:35 +0300

    Выполнение ДЗ №3

---
+++ Task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23.
+// Напишите программу, которая принимает на вход число (N) и выдаёт 
+// таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+int num, cube;
+
+cube = 1;
+
+Console.Write("Введите число: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+while (cube < num + 1)
+{
+  cube * cube;
+  Console.Write()
+}
